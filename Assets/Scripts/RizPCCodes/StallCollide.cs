d7ffde76501f737db2c9bc41119d29caa36a2259
Rejuanul Huq Sanny <rejuanulhuq@gmail.com>
2024-05-22 17:03:39 +0600

    tray added

--- Assets/Scripts/RizPCCodes/StallCollide.cs
+++ Assets/Scripts/RizPCCodes/StallCollide.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
 public class StallCollide : MonoBehaviour
 {
+    PlayerInput playerInput;
+    public GameObject Tray;
     bool inTrigger;
     void Start(){
         inTrigger = false;
+        playerInput = GetComponent<PlayerInput>();
     }
     void OnTriggerEnter(Collider collision){
         if(collision.CompareTag("Stalls")){
@@ -23,8 +27,17 @@ public class StallCollide : MonoBehaviour
     }
     
     void ElivateTray(){
-        if(Input.GetKey(key: KeyCode.G))
-            Debug.Log("xx");
-        
+        if(Input.GetKey(KeyCode.F)){
+            Tray.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            playerInput.enabled = false;
+        }
+    }
+
+
+    public void DeactiveTray(){
+        Tray.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        playerInput.enabled = true;
     }
 }
